152b23bc15f34015402cde1510a897274db4e52e
nichoth <nichoth@gmail.com>
2015-05-22 21:25:03 -0700

    working on react

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var slug = require('slug');
+var npmconf = require('npmconf');
+var latest = require('latest-version');
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
@@ -10,6 +12,7 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   prompting: function () {
+    var self = this;
     var done = this.async();
 
     // Have Yeoman greet the user.
@@ -39,9 +42,25 @@ module.exports = yeoman.generators.Base.extend({
       message: 'Dev Server?',
       choices: [{
         name: 'ecstatic',
-        value: 'includeEcstatic',
+        value: 'ecstatic',
         checked: false
       }]
+    },
+    {
+      type: 'checkbox',
+      name: 'devDeps',
+      message: 'Other stuff',
+      choices: [{
+        name: 'react',
+        value: 'react',
+        checked: false
+      }]
+    },
+    {
+      type: 'confirm',
+      name: 'installDeps',
+      message: 'Install dependencies?',
+      default: true
     }];
 
     this.prompt(prompts, function (props) {
@@ -49,11 +68,25 @@ module.exports = yeoman.generators.Base.extend({
       this.description = props.description;
       this.mainFile = props.mainFile;
       this.appNameSlug = slug(props.appName);
-      this.includeEcstatic = props.devServer.indexOf('includeEcstatic') !== -1;
-      this.includeServer = this.includeEcstatic;
+      this.devDeps = props.devDeps.concat(props.devServer);
+      this.includeServer = props.devServer.length > 0;
+      this.installDeps = props.installDeps;
+      this.transforms = [];
+      if (props.devDeps.indexOf('react') > -1) {
+        this.devDeps.push('reactify');
+        this.transforms.push('reactify');
+      }
       done();
     }.bind(this));
+
+    // npmconf.load({}, function(err, conf) {
+    //   self.authorName = conf.get('init.author.name');
+    //   self.website = conf.get('init.author.url');
+    //   self.authorEmail = conf.get('init.author.email');
+    //   self.github = conf.get('init.author.github');
+    // });
   },
+
 
   writing: {
     app: function () {
@@ -100,11 +133,8 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   install: function () {
-    // this.installDependencies({
-    //   skipInstall: this.options['skip-install']
-    // });
-    if (this.includeEcstatic) {
-      this.npmInstall(['ecstatic'], {saveDev: true});
+    if (this.installDeps) {
+      this.npmInstall(this.devDeps, {saveDev: true});
     }
   }
 });
